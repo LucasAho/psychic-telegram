170fd50ad90a3eff123273e7f54168b21f61d6b4
Lucas Aho <aholucas11@gmail.com>
2020-02-26 15:02:07 -0700

    Updates

---
+++ precache-manifest.e6f18f3dc957b94a71937b696bb99f69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99fabf5b61784bc34d7fa84a3b60c4b6",
+    "url": "/psychic-telegram/index.html"
+  },
+  {
+    "revision": "39dd9b0f705fb332664c",
+    "url": "/psychic-telegram/static/css/main.ef521792.chunk.css"
+  },
+  {
+    "revision": "7abe1da33cffc1aeccb0",
+    "url": "/psychic-telegram/static/js/2.2eeb62ad.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/psychic-telegram/static/js/2.2eeb62ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39dd9b0f705fb332664c",
+    "url": "/psychic-telegram/static/js/main.85b7f7a2.chunk.js"
+  },
+  {
+    "revision": "bf38a5b724b83af86369",
+    "url": "/psychic-telegram/static/js/runtime-main.1157fd18.js"
+  }
+]);
